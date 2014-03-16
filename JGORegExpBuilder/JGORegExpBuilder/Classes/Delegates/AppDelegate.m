880d86f6e37ec11544f57a0daffd860ae32d3786
Jan Gorman <gorman.jan@gmail.com>
2014-03-16 18:09:26 +0100

    Initial commit

---
+++ JGORegExpBuilder/JGORegExpBuilder/Classes/Delegates/AppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.m
+//  JGORegExpBuilder
+//
+//  Created by Jan Gorman on 3/16/14
+//  Copyright (c) 2014 Schnaub. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@end
---
+++ JGORegExpBuilder/JGORegExpBuilder/Resources/Other-Sources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JGORegExpBuilder
+//
+//  Created by Jan Gorman on 3/16/14
+//  Copyright (c) 2014 Schnaub. All rights reserved.
+//
+
+@import UIKit;
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
