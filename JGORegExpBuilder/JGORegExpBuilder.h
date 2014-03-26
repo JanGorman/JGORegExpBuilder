1666c8bee73fd702b231da99387147ba878689b5
Jan Gorman <gorman.jan@gmail.com>
2014-03-26 20:46:08 +0100

    new project

---
+++ JGORegExpBuilder/JGORegExpBuilder.h
@@ -0,0 +1,13 @@
+//
+//  JGORegExpBuilder.h
+//  JGORegExpBuilder
+//
+//  Created by Jan Gorman on 26/03/14.
+//  Copyright (c) 2014 Jan Gorman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JGORegExpBuilder : NSObject
+
+@end
---
+++ JGORegExpBuilder/JGORegExpBuilder.m
@@ -0,0 +1,13 @@
+//
+//  JGORegExpBuilder.m
+//  JGORegExpBuilder
+//
+//  Created by Jan Gorman on 26/03/14.
+//  Copyright (c) 2014 Jan Gorman. All rights reserved.
+//
+
+#import "JGORegExpBuilder.h"
+
+@implementation JGORegExpBuilder
+
+@end
